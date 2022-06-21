3fb2750200750d09ca6ed4d5f5d6b8fe55af87e7
Adrian Burgos <adrian.burgos.colas@gmail.com>
2022-06-20 22:16:38 -0400

    fixed login interface

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -52,13 +52,23 @@ function Login() {
   return (
     <div className="text-center text-stone-800 bg-stone-200">
       <h2 className="text-2xl font-bold flex-1">
-        {register ? "Register" : "Please LogIn"}
+        {register ? (
+          <div>
+            <p>Register</p>
+            <p className="text-sm font-normal text-stone-600">to create a new account</p>
+          </div>
+        ) : (
+          <div>
+            <p>Please LogIn</p>
+            <p className="text-sm font-normal text-stone-600">if you have an account</p>
+          </div>
+        )}
       </h2>
       <div className="text-red-800 font-medium">
         {/user/i.test(token.message) ? token.message : " "}
       </div>
       <form
-        className="w-80 mx-auto my-5 py-5 border-4 border-stone-300 rounded-xl"
+        className="w-80 mx-auto my-5 py-5 border-4 border-stone-400 rounded-xl"
         onSubmit={handleSubmit}
       >
         <label>
@@ -88,7 +98,7 @@ function Login() {
         {register && pass !== "" ? repeatPass : ""}
         <div>
           <button
-            className="bg-sky-700 hover:bg-sky-900 transition-all font-bold text-stone-200 p-2 m-5 rounded-xl"
+            className="bg-sky-700 hover:bg-sky-900 border-4 border-sky-700 hover:border-sky-900 transition-all font-bold text-stone-200 p-2 m-5 rounded-xl"
             disabled={register ? pass !== checkpass || pass === "" : false}
             type="submit"
           >
@@ -96,12 +106,18 @@ function Login() {
           </button>
         </div>
       </form>
+      <p className="text-sm font-normal text-stone-600">or</p>
       <button
-        className=" hover:text-stone-600 transition-all font-bold text-stone-800 p-2 rounded-xl"
+        className="border-4 border-stone-400 hover:bg-stone-400 transition-all font-bold text-stone-800 p-2 rounded-xl"
         onClick={() => setRegister((r) => !r)}
       >
         {register ? "Login" : "Register"}
       </button>
+      <p>{register ? (
+        <p className="text-sm font-normal text-stone-600">if you have an account</p>
+      ) : (
+        <p className="text-sm font-normal text-stone-600">to create a new accout</p>
+      )}</p>
     </div>
   );
 }
