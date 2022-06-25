92f27feb10ff9da966ebd0b90248360f101cfd24
Adrian Burgos <adrian.burgos.colas@gmail.com>
2022-06-24 22:27:31 -0400

    added spanish language to app title

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ import NoPage from "./Components/NoPage/NoPage";
 import Login from "./Components/Login/Login";
 import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
 import { AuthProvider } from "./Components/AuthProvider/AuthProvider";
+import { useEffect, useState } from "react";
 
 var userLang = navigator.language || navigator.userLanguage;
 
 function App() {
+  const [title, setTitle] = useState("");
+  useEffect(() => {
+    const appTitle = userLang === "es-ES" ? "Contador Pro" : "Pro Counter";
+    setTitle(appTitle);
+    document.title = appTitle;
+  });
   return (
     <AuthProvider>
       <div className="container mx-auto my-0 py-2 text-center text-stone-800 bg-stone-200 min-h-screen">
-        <h1 className="text-3xl font-bold m-3">{
-	      	userLang === "es-ES"?
-		"Contador Pro":
-		"Pro Counter"
-	      }</h1>
+        <h1 className="text-3xl font-bold m-3">{}</h1>
         <Routes>
           <Route element={<Layout />}>
             <Route index element={<Login />} />
@@ -42,12 +45,7 @@ function App() {
           </Route>
         </Routes>
         <footer className="text-xs mt-12">
-		{
-
-			userLang === "es-ES"?
-			"Escrito por ":
-			"Coded by "
-		}
+          {userLang === "es-ES" ? "Escrito por " : "Coded by "}
           <a
             className="text-sky-700 hover:underline hover:decoration-solid transition-all"
             href="https://adrianburgoscolas.github.io/portfolio/"
